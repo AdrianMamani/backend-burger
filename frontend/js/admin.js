e84c5aac465dc704197b896cdc9c6367c6d40e83
AdrianMamani <adrian.12mamani2005@gmail.com>
2025-09-27 01:16:02 -0500

    fix: Eliminación de carpetas y archivos innecesarios. Solo queda el backend.

--- frontend/js/admin.js
+++
@@ -1,4 +0,0 @@
-function logout() {
-    localStorage.removeItem('loggedIn');
-    location.hash = '#login';
-}
--- frontend/src/router.jsx
+++
@@ -1,4 +0,0 @@
-import Reclamaciones from './pages/Reclamaciones';
-
-// dentro del <Routes>
-<Route path="/reclamaciones" element={<Reclamaciones />} />
